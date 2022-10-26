88826d842b0c27eb4eecca25b16f9d359270bd20
Lois Wells <lois.wells@raspberrypi.org>
2022-10-26 14:57:27 +0100

    Added blurb and ability to order by columns

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,14 @@
 class BooksController < ApplicationController
 
+  STATUS_SORT_ORDER = ['Yes', 'In progress', 'No']
+
   def index
-    @books = filter(Book.order('title ASC'))
+    if params[:sort] == 'has_been_read'
+      @books = filter(Book.all).in_order_of(:has_been_read, STATUS_SORT_ORDER)
+    else
+      @books = filter(Book.all).sort_by{|book| book[params[:sort] || :title]}
+    end
+    @search_term = filter_params[:search_term]
   end
 
   def show
@@ -46,7 +53,7 @@ class BooksController < ApplicationController
 
   private
     def book_params
-      params.require(:book).permit(:title, :subtitle, :author, :publisher, :publication_date, :number_of_pages, :photo, :year_first_published, :has_been_read)
+      params.require(:book).permit(:title, :subtitle, :author, :publisher, :publication_date, :number_of_pages, :photo, :year_first_published, :blurb, :has_been_read)
     end
 
     def filter_params
@@ -57,6 +64,7 @@ class BooksController < ApplicationController
       return relation if filter_params.empty?
       relation.where("lower(title) LIKE (?)", "%#{filter_params[:search_term]}%")
       .or(relation.where("lower(author) LIKE (?)", "%#{filter_params[:search_term]}%"))
+      .or(relation.where("lower(blurb) LIKE (?)", "%#{filter_params[:search_term]}%"))
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_151445) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_26_132954) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_151445) do
     t.datetime "updated_at", null: false
     t.integer "year_first_published"
     t.string "has_been_read", default: "No"
+    t.string "blurb"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
