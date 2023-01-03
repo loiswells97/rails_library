53c224f85d5772c004d72b14aa30ec01a7fa6d13
Lois Wells <lois.wells@raspberrypi.org>
2023-01-03 17:23:04 +0000

    Allow searching of authors on authors page

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,9 +1,16 @@
 class AuthorsController < ApplicationController
   def index
-    @authors = Author.order(:surname)
+    search_term = filter_params[:search_term]
+    filtered_authors = Author.where("lower(first_name || ' ' || surname) LIKE (?)", "%#{search_term}%")
+    @authors = search_term.nil? ? Author.order(:surname) : filtered_authors.order(:surname)
   end
 
   def show
     @author = Author.find(params[:id])
   end
+
+  private
+    def filter_params
+      params.fetch(:filter, {}).permit(:search_term)
+    end
 end
