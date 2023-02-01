d7f64ed4e7d223badbaaaa4b9cccb7c76b752bd4
Lois Wells <lois.wells@raspberrypi.org>
2023-02-01 17:42:45 +0000

    improving multiselect option presentation

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,8 @@ class Book < ApplicationRecord
   accepts_nested_attributes_for :series
   has_and_belongs_to_many :lists
   has_one_attached :photo, :dependent => :destroy
+
+  def title_and_author
+    "#{title} (#{author.first_name} #{author.surname})"
+  end
 end
