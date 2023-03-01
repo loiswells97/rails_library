d989fa84eff050b65372c37ab801c272b8126ab5
Lois Wells <lois.wells@raspberrypi.org>
2023-03-01 17:12:46 +0000

    Adding recommendation helper test for rogue suggestions

--- spec/helpers/recommendations_helper_spec.rb
+++ spec/helpers/recommendations_helper_spec.rb
@@ -25,4 +25,43 @@ RSpec.describe RecommendationsHelper, type: :helper do
       expect(helper.related_books([book3])).to include(book4)
     end
   end
+
+  describe 'rogue_suggestions' do
+    let(:recently_read_book) { create(:book, has_been_read: 'Yes', date_finished_reading: 1.day.ago) }
+    let(:unrecently_read_book) { create(:book, has_been_read: 'Yes', date_finished_reading: 2.years.ago) }
+    let(:current_book) { create(:book, has_been_read: 'In progress') }
+
+    before do
+      helper.cookies[:rogue_wait] = 1
+    end
+
+    it 'does not suggest read books' do
+      expect(rogue_suggestions).not_to include(recently_read_book)
+      expect(rogue_suggestions).not_to include(unrecently_read_book)
+    end
+
+    it 'does not suggest current books' do
+      expect(rogue_suggestions).not_to include(current_book)
+    end
+
+    it 'does not suggest books related to current books' do
+      book = create(:book, author: current_book.author, has_been_read: 'No')
+      expect(rogue_suggestions).not_to include(book)
+    end
+
+    it 'does not suggest books related to recently read books' do
+      book = create(:book, author: recently_read_book.author, has_been_read: 'No')
+      expect(rogue_suggestions).not_to include(book)
+    end
+
+    it 'suggests books related to unrecently read books' do
+      book = create(:book, author: unrecently_read_book.author, has_been_read: 'No')
+      expect(rogue_suggestions).to include(book)
+    end
+
+    it 'suggests unread books that are unrelated to read and current books' do
+      book = create(:book, has_been_read: 'No')
+      expect(rogue_suggestions).to include(book)
+    end
+  end
 end
