c393536adb2b832b2a8debd75272aad6e5a9fd35
Lois Wells <lois.wells@raspberrypi.org>
2023-03-15 17:30:44 +0000

    Adding some relatedness methods on books model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
+require 'books_helper'
+
 class Book < ApplicationRecord
   belongs_to :author
   belongs_to :series, optional: true
@@ -9,4 +11,30 @@ class Book < ApplicationRecord
   def title_and_author
     "#{title} (#{author.full_name})"
   end
+
+  def related
+    (related_by_author + related_by_series + related_by_list).uniq
+  end
+
+  def related_by_author
+    author.books - [self]
+  end
+
+  def related_by_series
+    series.books - [self]
+  end
+
+  def related_by_list
+    lists.map { |list| list.books  - [self] }.flatten.uniq
+  end
+
+  def is_related?(book)
+    related.include?(book)
+  end
+
+  def relatedness_to(book)
+    related_by_list_with_duplicates = lists.map { |list| list.books  - [self] }.flatten
+    (related_by_author + related_by_list + related_by_list_with_duplicates).count(book)
+  end
 end
+
